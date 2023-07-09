7d13229f6ad8c64d5a0bf342275a2aa8b875101c
Mohsen Araghi <araghi13@outlook.com>
2023-07-09 19:58:49 +0330

    initial commit
    Co-Authored-By: Mohammad Pirzadeh <85078384+mohammadpirzadeh@users.noreply.github.com>

---
+++ Controller-app/RyuApp.py
@@ -0,0 +1,24 @@
+from ryu.base import app_manager
+from ryu.controller import ofp_event
+from ryu.controller.handler import MAIN_DISPATCHER
+from ryu.controller.handler import set_ev_cls
+from ryu.ofproto import ofproto_v1_0
+from ryu.lib.mac import haddr_to_bin
+from ryu.lib.packet import packet
+from ryu.lib.packet import ethernet
+from ryu.lib.packet import ether_types
+
+class PacketLisner(app_manager.RyuApp):
+
+    OFP_VERSIONS = [ofproto_v1_0.OFP_VERSION]
+
+    @set_ev_cls(ofp_event.EventOFPPacketIn, MAIN_DISPATCHER)
+    def _packet_in_handler(self, ev):
+        msg = ev.msg
+        datapath = msg.datapath
+        ofproto = datapath.ofproto
+
+        pkt = packet.Packet(msg.data)
+        eth = pkt.get_protocol(ethernet.ethernet)
+        ipv4_pkt = pkt.get_protocol(ipv4.ipv4)
+        print(ipv4_pkt.tos)
